40688f8f7913cb6c8cbda64bcf634eab23007cd9
joliu <ljo0412@live.comm>
2018-09-13 15:55:56 +0800

    Spring MVC项目快速搭建
    使用servlet3.0+无web.xml配置方式

---
+++ src/main/java/com/typh/highlight_springmvc4/WebInitializer.java
@@ -0,0 +1,26 @@
+package com.typh.highlight_springmvc4;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletRegistration.Dynamic;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/** 提供servlet3.0的接口，替代web.xml */
+public class WebInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx =
+                new AnnotationConfigWebApplicationContext();
+        ctx.register(MyMvcConfig.class);
+        ctx.setServletContext(servletContext);
+
+        // 注册DispaServlet
+        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
---
+++ src/main/resources/views/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ljo04
+  Date: 2018/9/13
+  Time: 14:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>First page</title>
+</head>
+<body>
+    <pre>
+        welcome to spring MVC world!!!!
+    </pre>
+</body>
+</html>
